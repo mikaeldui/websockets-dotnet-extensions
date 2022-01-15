291a54321e174a8f9bae77454e5779a68c2e84a1
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-15 06:06:30 +0100

    Added ClientWebSocketExtensions.

---
+++ DotNet.Net.WebSockets.Extensions/ClientWebSocketExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Net.WebSockets
+{
+    public static class ClientWebSocketExtensions
+    {
+        public static async Task ConnectAsync(this ClientWebSocket client, string uri, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrEmpty(uri))
+                throw new ArgumentNullException(nameof(uri));
+
+            await client.ConnectAsync(new Uri(uri), cancellationToken);
+        }
+    }
+}
